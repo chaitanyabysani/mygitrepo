207ff2a86f5bb5e21aec9613d9d92c3e821427e2
chaitanya <chaitanya@DESKTOP-SD1JJBJ>
2019-08-09 00:34:39 +0530

    Test Commit

---
+++ CI/src/main/java/ContiniousIntegration/CI/App.java
@@ -0,0 +1,13 @@
+package ContiniousIntegration.CI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
